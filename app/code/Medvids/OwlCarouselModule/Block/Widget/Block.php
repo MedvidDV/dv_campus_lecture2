3dc1ddcf484d6be82d272b34453987f3894ea0a5
Sergii Medvid <sergiim@default-value.com>
2020-01-14 16:19:15 +0200

    added owlcarousel module

---
+++ app/code/Medvids/OwlCarouselModule/Block/Widget/Block.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Medvids\OwlCarouselModule\Block\Widget;
+
+class Block extends \Magento\Cms\Block\Widget\Block
+{
+    protected $_template = 'Medvids_OwlCarouselModule::widget/owlcarousel.phtml';
+
+}
---
+++ app/code/Medvids/OwlCarouselModule/view/frontend/requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+    map: {
+        '*': {
+            owlCarouselSlider: 'Medvids_OwlCarouselModule/js/owlcarousel-slider'
+        }
+    },
+    paths: {
+        'owlcarousel': 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min'
+    },
+    shim: {
+        'owlcarousel': {
+            deps: ['jquery']
+        }
+    }
+};
